9b37a9034f420634487018611151552474f579a0
stefanie <stefanie.m@aluno.ifsc.edu.br>
2019-01-02 18:54:54 -0200

    Initial commit

---
+++ app/src/main/java/engtelecom/poo/stefanie/job/MainActivity.java
@@ -0,0 +1,49 @@
+package engtelecom.poo.stefanie.job;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void telaCombustivel(View view)
+    {
+        // OBJETO QUE DISPARA ACTIVITY NA TELA
+        Intent messageIntent = new Intent(this, CombustivelActivity.class);
+
+        startActivityForResult(messageIntent,20);
+    }
+
+    public void telaTrabalho(View view)
+    {
+        // OBJETO QUE DISPARA ACTIVITY NA TELA
+        Intent messageIntent = new Intent(this, TrabalhoActivity.class);
+
+        startActivityForResult(messageIntent,20);
+    }
+
+    public void telaDescontos(View view)
+    {
+        // OBJETO QUE DISPARA ACTIVITY NA TELA
+        Intent messageIntent = new Intent(this, DescontosActivity.class);
+
+        startActivityForResult(messageIntent,20);
+    }
+
+    public void telaBrutoLiquido(View view)
+    {
+        // OBJETO QUE DISPARA ACTIVITY NA TELA
+        Intent messageIntent = new Intent(this, BrutoLiquidoActivity.class);
+
+        startActivityForResult(messageIntent,20);
+    }
+}
---
+++ app/src/main/java/engtelecom/poo/stefanie/job/TrabalhoActivity.java
@@ -0,0 +1,13 @@
+package engtelecom.poo.stefanie.job;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrabalhoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trabalho);
+    }
+}
