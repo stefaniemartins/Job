35cd3c5360e928c86ac942acfbdc84f8c42f99c9
stefanie <stefanie.m@aluno.ifsc.edu.br>
2019-01-24 23:05:00 -0200

    CombustivelActivity está com cálculo funcionando, aceitando checkbox, todos os valores e nome do posto.

--- app/src/main/java/engtelecom/poo/stefanie/job/CombustivelActivity.java
+++ app/src/main/java/engtelecom/poo/stefanie/job/CombustivelActivity.java
@@ -2,12 +2,44 @@ package engtelecom.poo.stefanie.job;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class CombustivelActivity extends AppCompatActivity {
+public class CombustivelActivity extends AppCompatActivity
+{
+    private EditText editPago;
+    private EditText editPreco;
+    private TextView textEsperado;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_combustivel);
+    }
+
+    public void calcular(View view)
+    {
+        editPago = findViewById(R.id.pago);
+        editPreco = findViewById(R.id.preco);
+        textEsperado = findViewById(R.id.esperado);
+
+        Button buttonCalcular = findViewById(R.id.calcular);
+
+        buttonCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                float valorPago = Float.parseFloat(editPago.getText().toString());
+                float valorPreco = Float.parseFloat(editPreco.getText().toString());
+
+                float valorCalculado = valorPago / valorPreco;
+                textEsperado.setText(valorCalculado + " litros");
+            }
+        });
     }
 }
--- app/src/main/java/engtelecom/poo/stefanie/job/MainActivity.java
+++ app/src/main/java/engtelecom/poo/stefanie/job/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
